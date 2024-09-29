a1003e876c48caf305d269eba49d3dcd726137ec
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-09-29 05:31:40 +0100

    Add merge sort function

--- script.js
+++ script.js
@@ -13,18 +13,52 @@ function fibs(n){
 // fibs(8);
 
 function fibRecursion(n) {
-   if(n == 1) {
+   if(n == 0) {
       return [0];
    } 
-   if(n == 2){
+   if(n == 1){
       return [0, 1]
    }
 
    let fibArray = fibRecursion(n - 1);
-   let nextArray = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+   let nextArray = fibArray[n - 1] + fibArray[n - 2];
 
    fibArray.push(nextArray);
    return fibArray;
    
 }
-console.log(fibRecursion(8));
+// console.log(fibRecursion(8));
+
+// for merge sort
+
+function mergeSort(array) {
+   if(array.length < 2) {
+      return array;
+   }
+
+   const middle = array.length / 2;
+   const left = array.slice(0, middle);
+   const right = array.slice(middle);
+
+   return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+   let i = 0;
+   let j = 0;
+
+   let sortedArray = [];
+
+   while(i < left.length && j < right.length) {
+      if(left[i] < right[j]) {
+         sortedArray.push(left[i]);
+         i++
+      } else {
+         sortedArray.push(right[j]);
+         j++
+      }
+   }
+   return sortedArray.concat(left.slice(i)).concat(right.slice(j));
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
